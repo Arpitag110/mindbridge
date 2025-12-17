11faa553e3561d8fffcb2f86f2df3650ab9bcdea
Arpitag110 <arpit.agarwal1612@gmail.com>
2025-12-17 14:26:56 +0530

    Connected server to MongoDB

--- server/index.js
+++ server/index.js
@@ -1,21 +1,22 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose"); // Import Mongoose
 const dotenv = require("dotenv");
+const app = express();
 
-// Load our environment variables (secrets)
 dotenv.config();
 
-// Middleware: Allows us to accept JSON data (like form submissions)
 app.use(express.json());
 
-// A simple Route (like a URL page) to test if the server works
+// CONNECT TO MONGODB
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB!"))
+  .catch((err) => console.error("MongoDB Connection Failed:", err));
+
 app.get("/", (req, res) => {
   res.send("MindBridge Server is Running!");
 });
 
-// Pick a port (door) for the server to listen on
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
